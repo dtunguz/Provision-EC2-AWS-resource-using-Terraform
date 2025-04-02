1e810e25c7b4810d5224cad5fa085810c9fddcf2
dtunguz <durdijatunguz@gmail.com>
2025-04-02 12:17:33 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"  # Or your desired version
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"  # e.g., "us-east-1"
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "example" {
+  ami                    = "ami-0ecf75a98fe8519d7"  # Replace with your desired AMI
+  instance_type          = "t2.micro"  # Or your desired instance type
+  key_name               = "test"  # Replace with your key pair name
+  }
