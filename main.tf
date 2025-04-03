f14756f0c5ffda856510f75cbb7f07b1e54cb648
dtunguz <durdijatunguz@gmail.com>
2025-04-03 11:17:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,6 +17,26 @@ resource "aws_instance" "example" {
   ami                    = "ami-0ecf75a98fe8519d7"  # Replace with your desired AMI
   instance_type          = "t2.micro"  # Or your desired instance type
   key_name               = "test"  # Replace with your key pair name
+  security_groups        = [aws_security_group.example.name]
 # Optional step if you want more than one EC2 instance to be provisioned
   count = 2
   }
+
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Allow SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Open to all IPs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
